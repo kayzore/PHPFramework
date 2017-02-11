941500d5b2a42895543420e018dc9aafdbf28bbd
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-02-11 20:07:21 +0100

    Ajout de la recupération dynamique des paramètres dans un fichier yaml

--- src/kayzore/bundle/ConfigurationBundle/KayParameters.php
+++ src/kayzore/bundle/ConfigurationBundle/KayParameters.php
@@ -3,13 +3,23 @@
 namespace kayzore\bundle\ConfigurationBundle;
 
 
+use Symfony\Component\Yaml\Yaml;
+
 class KayParameters
 {
     private $pathConfig = 'app/config/';
     private $pathErrorView  = 'src/kayzore/bundle/ErrorBundle/';
 
     public function __construct() {
+        // PROJECT INFORMATIONS
+        define('RACINE_WEB', '/PHPFramework/');
         define('DIR_VIEW_ERROR', $_SERVER['DOCUMENT_ROOT'] . RACINE_WEB . $this->pathErrorView);
         define('DIR_CONFIG', $_SERVER['DOCUMENT_ROOT'] . RACINE_WEB . $this->pathConfig);
+        $config = Yaml::parse(file_get_contents(DIR_CONFIG . 'parameters.yml'));
+        // DATABASE INFORMATIONS
+        define('DB_HOST', $config['parameter']['db_host']);
+        define('DB_NAME', $config['parameter']['db_name']);
+        define('DB_USER', $config['parameter']['db_user']);
+        define('DB_PASS', $config['parameter']['db_pass']);
     }
 }
