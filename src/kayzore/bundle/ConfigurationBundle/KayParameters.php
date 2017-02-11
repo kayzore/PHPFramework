7bfb1c4c05812f3388d654cc5829748610c19c0e
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-02-11 17:31:11 +0100

    update global parameter file

--- src/kayzore/bundle/ConfigurationBundle/KayParameters.php
+++ src/kayzore/bundle/ConfigurationBundle/KayParameters.php
@@ -5,14 +5,11 @@ namespace kayzore\bundle\ConfigurationBundle;
 
 class KayParameters
 {
-    private $pathView;
-    private $pathConfig;
+    private $pathConfig = 'app/config/';
     private $pathErrorView  = 'src/kayzore/bundle/ErrorBundle/';
 
     public function __construct() {
         define('DIR_VIEW_ERROR', $_SERVER['DOCUMENT_ROOT'] . RACINE_WEB . $this->pathErrorView);
-
-        define('DIR_VIEW', $_SERVER['DOCUMENT_ROOT'] . RACINE_WEB . $this->pathView);
         define('DIR_CONFIG', $_SERVER['DOCUMENT_ROOT'] . RACINE_WEB . $this->pathConfig);
     }
 }
