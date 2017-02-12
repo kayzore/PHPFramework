8693eee427f705409a719e4e33571adfc119ec2e
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-02-12 12:39:13 +0100

    Gestion des erreurs pour les noms des bundles

--- src/kayzore/bundle/ControllerBundle/Controller/Controller.php
+++ src/kayzore/bundle/ControllerBundle/Controller/Controller.php
@@ -3,6 +3,7 @@
 namespace kayzore\bundle\ControllerBundle\Controller;
 
 use app\AppKernel;
+use kayzore\bundle\ServicesBundle\Model\ServiceException;
 use Symfony\Component\Yaml\Yaml;
 use Twig_Environment;
 use Twig_Extension_Debug;
@@ -21,12 +22,18 @@ class Controller
             $this->bundles[$name] = $bundlePath;
         }
         // Instanciation de Twig
-        $loader = new Twig_Loader_Filesystem(
-            RACINE_WEB . $this->bundles[$nameBundle] . 'Ressources\Views\\'
-        );
-        // Paramètres twig
-        $config = Yaml::parse(file_get_contents(DIR_CONFIG . 'config.yml'));
-        $this->twig = new Twig_Environment($loader, $config['twig']);
-        $this->twig->addExtension(new Twig_Extension_Debug());
+        if (empty($this->bundles[$nameBundle])) {
+            $errorMessage = "Aucun bundle portant le nom $nameBundle n'a été trouvé, "
+                . "vérifier la délcaration dans l'AppKernel, ou le nom du bundle dans le controller";
+             new ServiceException(404, $errorMessage);
+        } else {
+            $loader = new Twig_Loader_Filesystem(
+                RACINE_WEB . $this->bundles[$nameBundle] . 'Ressources\Views\\'
+            );
+            // Paramètres twig
+            $config = Yaml::parse(file_get_contents(DIR_CONFIG . 'config.yml'));
+            $this->twig = new Twig_Environment($loader, $config['twig']);
+            $this->twig->addExtension(new Twig_Extension_Debug());
+        }
     }
 }
