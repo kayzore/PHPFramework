7256ea07a6517b90e6dd9c487a2f2dda2d39864c
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-02-12 13:23:37 +0100

    Fix #1 Fatal error: Uncaught exception 'Twig_Error_Loader' for the ServiceException

--- src/kayzore/bundle/ConfigurationBundle/KayParameters.php
+++ src/kayzore/bundle/ConfigurationBundle/KayParameters.php
@@ -8,14 +8,14 @@ use Symfony\Component\Yaml\Yaml;
 class KayParameters
 {
     private $pathConfig = 'app/config/';
-    private $pathErrorView  = 'src/kayzore/bundle/ServicesBundle/Ressources/Views/';
+    private $pathErrorView  = 'kayzore/bundle/ServicesBundle/Ressources/Views/';
 
     public function __construct() {
         // PROJECT INFORMATIONS
         define('DIR_CONFIG', RACINE_WEB . $this->pathConfig);
         $config = Yaml::parse(file_get_contents(DIR_CONFIG . 'parameters.yml'));
-
-        define('DIR_VIEW_ERROR', RACINE_WEB . $this->pathErrorView);
+        define('DIR_VIEW_ERROR', RACINE_WEB . DIR_BUNDLE . $this->pathErrorView);
+        
         // DATABASE INFORMATIONS
         define('DB_HOST', $config['parameter']['db_host']);
         define('DB_NAME', $config['parameter']['db_name']);
